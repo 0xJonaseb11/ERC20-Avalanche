fd4d96e132f28a4322dd041a53e7b149e6d2793d
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 04:01:22 +0200

    updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,27 +10,17 @@ module.exports = {
     avalanche: {
       url: "https://api.avax-test.network/ext/bc/C/rpc",
       blockExplorerUrls: "https://testnet.snowtrace.io/",
-
+      
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+ 
       chainId: "0xA869",
       chainName: "Avalanche Testnet C-Chain",
       nativeCurrency: {
       name: "Avalanche",
       symbol: "AVAX",
       decimals: 18,
-
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    }
-  }
-
-};
-export const AVALANCHE_TESTNET_PARAMS = {
-  chainId: "0xA869",
-  chainName: "Avalanche Testnet C-Chain",
-  nativeCurrency: {
-    name: "Avalanche",
-    symbol: "AVAX",
-    decimals: 18,
+    },
+   },
   },
-  rpcUrls: ["https://api.avax-test.network/ext/bc/C/rpc"],
-  blockExplorerUrls: ["https://testnet.snowtrace.io/"],
+
 };
