8037a258cbfaf9df3e1e8137aa79b49193f34338
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 03:59:15 +0200

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,26 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+  },
+  networks: {
+    hardhat: {},
+    avalanche: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      blockExplorerUrls: "https://testnet.snowtrace.io/",
+
+      chainId: "0xA869",
+      chainName: "Avalanche Testnet C-Chain",
+      nativeCurrency: {
+      name: "Avalanche",
+      symbol: "AVAX",
+      decimals: 18,
+
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    }
+  }
+
 };
 export const AVALANCHE_TESTNET_PARAMS = {
   chainId: "0xA869",
