0519cee37080b0b312826d3b8260b6503111fcc2
jonas-sebera <sebejaz99@gmail.com>
2023-11-26 03:53:55 +0200

    update deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,3 +4,14 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
 };
+export const AVALANCHE_TESTNET_PARAMS = {
+  chainId: "0xA869",
+  chainName: "Avalanche Testnet C-Chain",
+  nativeCurrency: {
+    name: "Avalanche",
+    symbol: "AVAX",
+    decimals: 18,
+  },
+  rpcUrls: ["https://api.avax-test.network/ext/bc/C/rpc"],
+  blockExplorerUrls: ["https://testnet.snowtrace.io/"],
+};
